66f49d14a5612944b707e9978fea22a371cedf4e
Hitesh Patel <hbp976@gmail.com>
2020-09-30 10:29:45 -0600

    outputing variables

--- hello-world.php
+++ hello-world.php
@@ -1,3 +1,25 @@
+This is before my PHP tag.
+
+
 <?php 
 
-echo "Hello, World!"; ?>
+//"Echo" and "print" are functions we can
+echo "Hello, World!";
+
+#This is also single line comment.
+
+/**
+        * This is multi line comments
+ */
+
+ //variable names start with a dollar sign and they are case sensitive.
+$studentName = "\nFahad";
+echo $studentName;
+
+$studentName2 = '\nDanish';
+echo $studentName2;
+
+echo '\n\n Two students in TechCareers are:\n $studentName\n$studentName2.'; 
+
+echo "\n\nTwo students in TechCareers are:\n $studentName\n$studentName2.";
+
